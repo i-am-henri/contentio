5e496f29a5265a70e60344c18df167beba823413
henri.me <henri.me@skiff.com>
2024-03-23 19:21:33 +0000

    add checks for the root of the project

--- script/index.ts
+++ script/index.ts
@@ -3,17 +3,30 @@ import chalk from "chalk";
 import inquirer from "inquirer";
 import terminalLink from "terminal-link";
 const version = "0.0.1";
-console.log(`${chalk.green(`next-docs, version ${version}, maintained by programmers like you`)} \n`);
+console.log(`${chalk.bgWhite(`next-docs, version ${version}`)} \n`);
+
+// checks if you on the root of your project
 await inquirer.prompt({
-    type: "list",
+    name: "root",
+    type: "confirm",
+    message: "Are you on the root of your project? (do you see a package.json file):",
+}).then((data) => {
+    if (data.root == false) {
+        console.log(chalk.bgRed("\n\nYou have to be on the root of your next project. Try to navigate to the root."))
+        process.exit(1)
+    }
+})
+
+// checks if you using the app directory in next, if not: the cli will return an error and stops
+await inquirer.prompt({
+    type: "confirm",
     message: "Are you using the app directory?",
-    choices: ["yes", "no"],
     name: "app_directory",
 }).then((data) => {
-    console.log(data)
-    if (data.app_directory == "no") {
-        console.log(chalk.bgRed("Sorry, but we aren't supporting the pages direcory yet.\n"))
+    if (data.app_directory == false) {
+        console.log(chalk.bgRed("\n\nSorry, but we aren't supporting the pages direcory yet."))
         console.log(`You can help us creating a supported version of this cli ${terminalLink("here", "https://github.com/i-am-henri/next-docs")}.`)
+        console.log('(https://github.com/i-am-henri/next-docs)')
         process.exit(1)
     }
 })
