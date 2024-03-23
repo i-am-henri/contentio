a79d18cda173593b0d675d562388be44e9ee0c4d
henri.me <henri.me@skiff.com>
2024-03-23 19:13:48 +0000

    set compiler options to module and add directory

--- script/index.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import chalk from "chalk";
-const version = "0.0.1";
-console.log(`
-${chalk.bgGray(`next-docs, version ${version}`)}
-`);
--- script/index.ts
+++ script/index.ts
@@ -1,7 +1,19 @@
 #!/usr/bin/env node
-import chalk from "chalk"
-import inquirer from "inquirer"
-const version: string = "0.0.1"
-console.log(`
-${chalk.bgGray(`next-docs, version ${version}`)}
-`)
+import chalk from "chalk";
+import inquirer from "inquirer";
+import terminalLink from "terminal-link";
+const version = "0.0.1";
+console.log(`${chalk.green(`next-docs, version ${version}, maintained by programmers like you`)} \n`);
+await inquirer.prompt({
+    type: "list",
+    message: "Are you using the app directory?",
+    choices: ["yes", "no"],
+    name: "app_directory",
+}).then((data) => {
+    console.log(data)
+    if (data.app_directory == "no") {
+        console.log(chalk.bgRed("Sorry, but we aren't supporting the pages direcory yet.\n"))
+        console.log(`You can help us creating a supported version of this cli ${terminalLink("here", "https://github.com/i-am-henri/next-docs")}.`)
+        process.exit(1)
+    }
+})
