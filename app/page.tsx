73048c90268b34c832b20476ef0b1db7d472a1af
henri.me <henri.me@skiff.com>
2024-03-24 11:57:10 +0000

    add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+import Img from "next/image"
+import Screenshot from "@/public/scrnsht.webp"
 export default function Home() {
   return (
     <div>
-      <section>
-        <h1>
-          Add content to your app easily.
+      <section className="min-h-screen flex flex-col items-center justify-center">
+        <h1 className="text-3xl text-center mt-[20vh]">
+          Add content to<br />your next-app
         </h1>
+        <p>
+          npx next-content
+        </p>
+        <Img className="rounded-[20px] border-4 border-[#ffffff1f] ring-[1px] ring-white " src={Screenshot} width={500} height={500} alt="a screenshot" />
       </section>
     </div>
   )
