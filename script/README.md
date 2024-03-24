db98b71390b3397a193b6543bfc952f69b5efc81
Henri <henri.me@skiff.com>
2024-03-24 18:45:30 +0100

    add a readme for describing the cli

--- script/README.md
+++ script/README.md
@@ -1,1 +1,2 @@
-# next-docs
+# next-content | add content to your nextjs app
+We make it possible to easy add content as Markdown or Mdx to your nextjs application.
---
+++ script/app/(content)/blog/[...slug]/blog.ts
@@ -0,0 +1,19 @@
+
+// file added by next-docs
+import { compileMDX } from "next-mdx-remote/rsc";
+import { notFound } from "next/navigation";
+import fs from "node:fs"
+export async function getMarkdown(slug: string): Promise<string> {
+    let raw: string = ""
+    await fs.promises.readFile("content/blog/" + slug + ".mdx", 'utf-8').then((data) => {
+        raw = data;
+    }).catch((err) => {
+        console.log(err.code)
+        if (err.code == "ENOENT") notFound()
+    })
+    if (raw == "") {
+        notFound()
+    }
+    return raw
+}
+            
