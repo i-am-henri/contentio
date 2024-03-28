ff6797d8b940782baa6d45a844b3bb64225eedb5
Henri <henri.me@skiff.com>
2024-03-28 20:16:20 +0100

    add functions for generating templates

---
+++ script/templates/script.ts
@@ -0,0 +1,23 @@
+/**
+ * Generate the template for the script. 
+ * Please provide the route-name!
+ * @param name string
+ */
+export const generateScriptTemplate = (name: string): string => {
+    return `// file added by next-docs
+    import { notFound } from "next/navigation";
+    import fs from "node:fs"
+    export async function getMarkdown(slug: string): Promise<string> {
+        let raw: string = ""
+        await fs.promises.readFile("content/${name}/" + slug + ".mdx", 'utf-8').then((data) => {
+            raw = data;
+        }).catch((err) => {
+            console.log(err.code)
+            if (err.code == "ENOENT") notFound()
+        })
+        if (raw == "") {
+            notFound()
+        }
+        return raw
+    }`
+}
