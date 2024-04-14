ed686392c7048d3b40ee44b69c80f1151f59358e
Henri <henri.me@skiff.com>
2024-04-14 21:19:58 +0200

    make all of the code working

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div>
       <section>
-        
+        <h2>
+          add content to your nextjs app
+        </h2>
       </section>
     </div>
   )
