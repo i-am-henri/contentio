b80a5c8ad92ed976ddf571f63189062bb91a4021
henri <henri.me@skiff.com>
2024-05-05 16:48:15 +0200

    add a grid layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,33 @@
-export default function Home() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="grid grid-cols-1 md:grid-cols-2 ">
-        <div className="col-span-2">
-          <h1>contentio</h1>
-        </div>
+import { cn } from "@/lib/utils"
+import { ReactNode } from "react"
 
+export default function Home() {
+  const GridComponent = ({children, className}: {children: ReactNode, className: string}) => {
+    return (
+      <div className={cn("flex flex-col border items-center justify-center", className)}>
+        {children}
+      </div>
+    )
+  }
+  return (
+    <div className="min-h-screen flex  justify-center">
+      <div className="grid md:w-[800px] md:auto-rows-[10rem] grid-cols-1 md:grid-cols-2 ">
+        <GridComponent className="col-span-1 md:col-span-2">
+          <h1 className="text-xl text-accent">contentio</h1>
+          <p>add content to your next application</p>
+        </GridComponent>
+        <GridComponent className="col-span-1">
+          hey
+        </GridComponent>
+        <GridComponent className="col-span-1">
+          I'm Henri
+        </GridComponent>
+        <GridComponent className="col-span-1">
+          hey
+        </GridComponent>
+        <GridComponent className="col-span-1">
+          I'm Henri
+        </GridComponent>
       </div>
     </div>
   )
