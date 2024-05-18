8360761e81a90d481442fb66a4c277c9d8d9f2b2
henri <henri.me@skiff.com>
2024-05-18 11:56:36 +0200

    add description for the file

--- cli/actions/add/index.ts
+++ cli/actions/add/index.ts
@@ -1,11 +1,21 @@
+/*
+    Contentio 2024
+    This file if for adding a new route to the nextjs project.
+*/
 import chalk from "chalk";
 
+
+
 export default async function add(path?: string) {
+    // welcome command
     console.log(`
 ${chalk.cyan("Contentio 2024")}
 
 Add a new route.
-${path? `\nAdding route ${path}.`: ""}
+${path? `\nAdding route "${path}".`: ""}
     `)
 
+    if (!path) {
+
+    }
 }
