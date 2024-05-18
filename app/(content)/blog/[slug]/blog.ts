5aa7578b7e0b73cdff6cb588dec642eb7ca476d2
henri <henri.me@skiff.com>
2024-05-18 18:32:54 +0200

    add content with the cli to the homepage

---
+++ app/(content)/blog/[slug]/blog.ts
@@ -0,0 +1,16 @@
+// file added by next-docs
+import { notFound } from "next/navigation";
+import fs from "node:fs"
+export async function getMarkdown(slug: string): Promise<string> {
+    let raw: string = ""
+    await fs.promises.readFile("content/blog/" + slug + ".mdx", 'utf-8').then((data) => {
+        raw = data;
+    }).catch((err) => {
+        console.log(err.code)
+        if (err.code == "ENOENT") notFound()
+    })
+    if (raw == "") {
+        notFound()
+    }
+    return raw
+}
