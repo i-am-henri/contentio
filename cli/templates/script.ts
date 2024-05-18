ee9df0ad10ed318fd184184a65b0386e178a21c5
henri <henri.me@skiff.com>
2024-05-18 12:15:56 +0200

    remove the whitespace

--- cli/templates/script.ts
+++ cli/templates/script.ts
@@ -5,19 +5,19 @@
  */
 export const generateScriptTemplate = (name: string): string => {
     return `// file added by next-docs
-    import { notFound } from "next/navigation";
-    import fs from "node:fs"
-    export async function getMarkdown(slug: string): Promise<string> {
-        let raw: string = ""
-        await fs.promises.readFile("content/${name}/" + slug + ".mdx", 'utf-8').then((data) => {
-            raw = data;
-        }).catch((err) => {
-            console.log(err.code)
-            if (err.code == "ENOENT") notFound()
-        })
-        if (raw == "") {
-            notFound()
-        }
-        return raw
-    }`
+import { notFound } from "next/navigation";
+import fs from "node:fs"
+export async function getMarkdown(slug: string): Promise<string> {
+    let raw: string = ""
+    await fs.promises.readFile("content/${name}/" + slug + ".mdx", 'utf-8').then((data) => {
+        raw = data;
+    }).catch((err) => {
+        console.log(err.code)
+        if (err.code == "ENOENT") notFound()
+    })
+    if (raw == "") {
+        notFound()
+    }
+    return raw
+}`
 }
