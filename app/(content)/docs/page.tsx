35a616e6fe3652481f9f2fa50ccedce2eb1e7fe0
henri <henri.me@skiff.com>
2024-05-18 21:16:27 +0200

    add a file-tree with the shadcn accordion

--- app/(content)/docs/page.tsx
+++ app/(content)/docs/page.tsx
@@ -3,13 +3,32 @@ import fs from "fs"
 import Viewer from "@/components/editor/Viewer";
 import "@/app/globals.css"
 import Link from "@/components/ui/link"
+import { FileTree, FileTreeContent, FileTreeItem, FileTreeTrigger, File } from "@/components/ui/file-tree";
+import { Folder } from "lucide-react";
 export default async function Docs() {
     const raw = (await fs.promises.readFile("content/docs/index.mdx")).toString()
     const withoutFrontmatter: string = raw.replace(/---[sS]*?---/, '');
 
     return <div className="bg-[#151515] min-h-screen text-white flex items-start justify-center">
-        <div className="w-[800px]">   
-            <MDXRemote  components={{Viewer, Link}}  source={withoutFrontmatter} />
+        <div className="w-[800px]">
+            <FileTree type="single" collapsible>
+                <FileTreeItem className="border-none outline-none p-0" value="item-1">
+                    <FileTreeTrigger className="p-0">app</FileTreeTrigger>
+                    <FileTreeContent>
+                        <FileTree type="single" collapsible>
+                            <FileTreeItem className="border-none  outline-none p-0" value="item-1">
+                                <FileTreeTrigger  className="p-0">Is it accessible?</FileTreeTrigger>
+                                <FileTreeContent>
+                                    <File >
+                                        index.tsx
+                                    </File>
+                                </FileTreeContent>
+                            </FileTreeItem>
+                        </FileTree>
+                    </FileTreeContent>
+                </FileTreeItem>
+            </FileTree>
+            <MDXRemote components={{ Viewer, Link }} source={withoutFrontmatter} />
         </div>
     </div>
 }
---
+++ components/ui/file-tree.tsx
@@ -0,0 +1,67 @@
+"use client"
+
+import * as React from "react"
+import * as AccordionPrimitive from "@radix-ui/react-accordion"
+import { ChevronDown, Folder, FileArchive  } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+const FileTree = AccordionPrimitive.Root
+
+const FileTreeItem = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Item>
+>(({ className, ...props }, ref) => (
+  <AccordionPrimitive.Item
+    ref={ref}
+    className={cn("border-b", className)}
+    {...props}
+  />
+))
+FileTreeItem.displayName = "AccordionItem"
+
+const FileTreeTrigger = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Header className="flex">
+    <AccordionPrimitive.Trigger
+      ref={ref}
+      className={cn(
+        "flex flex-1 items-center justify-start font-medium transition-all hover:underline ",
+        className
+      )}
+      {...props}
+    >
+      <Folder id="folder" className="mr-2 h-4 w-4  "/>
+      {children}
+    </AccordionPrimitive.Trigger>
+  </AccordionPrimitive.Header>
+))
+FileTreeTrigger.displayName = AccordionPrimitive.Trigger.displayName
+
+const FileTreeContent = React.forwardRef<
+  React.ElementRef<typeof AccordionPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Content>
+>(({ className, children, ...props }, ref) => (
+  <AccordionPrimitive.Content
+    ref={ref}
+    className="overflow-hidden ml-3 text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    {...props}
+  >
+    <div className={cn("pb-4 pt-0", className)}>{children}</div>
+  </AccordionPrimitive.Content>
+))
+
+FileTreeContent.displayName = AccordionPrimitive.Content.displayName
+const File = ({children}: {children: React.ReactNode}) => {
+  return (
+    <div
+      className={"flex flex-1 items-center justify-start font-medium transition-all hover:underline "}
+    >
+      <FileArchive id="folder" className="mr-2 h-4 w-4  "/>
+      {children}
+    </div>
+  )
+}
+export { FileTree, FileTreeItem, FileTreeTrigger, FileTreeContent, File }
