bfa3dc973d12d3b78f1bedcd72f8a457b484ba66
henri <henri.me@skiff.com>
2024-05-18 11:34:20 +0200

    add a custom error method.

---
+++ cli/functions/error.ts
@@ -0,0 +1,19 @@
+import chalk from "chalk"
+import { terminalLinkSupported } from "./link.js"
+import terminalLink from "terminal-link"
+
+export function error({
+    message,
+    code
+}: {
+    message: string,
+    code: number
+}) {
+    console.error(`
+${chalk.red(`\n[error] ${message}`)}
+    
+Because of this error, the program won't be continued.
+Do you think it's a bug? Please create an issue ${chalk.blue(`${terminalLinkSupported? terminalLink("here", "https://git.new/contentio"): "here: https://git.new/contentio"}`)}.
+`)
+    process.exit(1)
+}
